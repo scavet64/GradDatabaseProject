1f1148e297b7c280e8596078df2e4dd47f83a5c9
jscavetta95 <jscavetta95@gmail.com>
2018-12-02 22:03:05 -0500

    Close #8

---
+++ product_shipment_view.sql
@@ -0,0 +1,19 @@
+DROP VIEW IF EXISTS `product_shipment_view`;
+CREATE VIEW `product_shipment_view` AS
+    SELECT 
+        (SELECT 
+                CONCAT(`c`.`first_name`, ' ', `c`.`last_name`)
+            FROM
+                `customer` `c`
+            WHERE
+                (`c`.`customer_id` = `o`.`customer_id`)) AS `customer`,
+        (SELECT 
+                `p`.`name`
+            FROM
+                `product` `p`
+            WHERE
+                (`p`.`product_id` = `op`.`product_id`)) AS `product`,
+        `o`.`shipment_date` AS `shipment_date`
+    FROM
+        (`order` `o`
+        JOIN `order_product` `op`)
