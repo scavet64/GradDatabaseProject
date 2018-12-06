1047d04953e3c869178611bab46ec76e4e718fa7
jscavetta95 <jscavetta95@gmail.com>
2018-12-05 21:25:26 -0500

    Revert Status Table and Update Views

--- db/views/unpurchased_wished_for_items_view.sql
+++ db/views/unpurchased_wished_for_items_view.sql
@@ -1,28 +1,29 @@
+DROP VIEW IF EXISTS `unpurchased_wished_for_items_view`;
 CREATE VIEW `unpurchased_wished_for_items_view` AS
     SELECT 
         (SELECT 
-                p.name
+                `p`.`name`
             FROM
-                product_view p
+                `product_view` `p`
             WHERE
-                p.product_id = w.product_id
-                    AND p.source = w.product_source) product,
+                ((`p`.`product_id` = `w`.`product_id`)
+                    AND (`p`.`source` = `w`.`product_source`))) AS `product`,
         (SELECT 
-                CONCAT(c.first_name, ' ', c.last_name)
+                CONCAT(`c`.`first_name`, ' ', `c`.`last_name`)
             FROM
-                customer_view c
+                `customer_view` `c`
             WHERE
-                c.customer_id = w.customer_id
-                    AND c.source = w.customer_source) customer
+                ((`c`.`customer_id` = `w`.`customer_id`)
+                    AND (`c`.`source` = `w`.`customer_source`))) AS `customer`
     FROM
-        wishlist w
+        `wishlist` `w`
     WHERE
-        CONCAT(product_id, source) NOT IN ((SELECT 
-                CONCAT(product_id, source)
+        (NOT (CONCAT(`w`.`product_id`, `w`.`product_source`) IN (SELECT 
+                CONCAT(`order_product`.`product_id`,
+                            `order_product`.`product_source`)
             FROM
-                `order` o
-                    JOIN
-                order_product USING (order_id)
+                (`order` `o`
+                JOIN `order_product` ON ((`o`.`order_id` = `order_product`.`order_id`)))
             WHERE
-                o.customer_id = w.customer_id
-                    AND o.customer_source = w.customer_source));
+                ((`o`.`customer_id` = `w`.`customer_id`)
+                    AND (`o`.`customer_source` = `w`.`customer_source`)))));
