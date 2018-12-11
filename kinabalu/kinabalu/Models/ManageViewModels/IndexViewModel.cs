56662493194ba324d879af92b0613f8faad5ec03
Vincent Scavetta <scavet64@students.rowan.edu>
2018-12-10 22:52:43 -0500

    Bring back Manage controller so it works now

--- kinabalu/kinabalu/Models/ManageViewModels/IndexViewModel.cs
+++ kinabalu/kinabalu/Models/ManageViewModels/IndexViewModel.cs
@@ -3,23 +3,25 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Kinabalu.Models;
 
 namespace IdentityDemo.Models.ManageViewModels
 {
     public class IndexViewModel
     {
-        public string Username { get; set; }
-
-        public bool IsEmailConfirmed { get; set; }
-
         [Required]
         [EmailAddress]
+        [Display(Name = "Email Address")]
         public string Email { get; set; }
 
-        [Phone]
-        [Display(Name = "Phone number")]
-        public string PhoneNumber { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
 
         public string StatusMessage { get; set; }
+
+        public List<CustomerAddress> Addresses { get; set; }
     }
 }
